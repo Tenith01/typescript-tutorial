69421a09febecf70bb415b15a5c0339a74489855
tenith <hasintha199@gmail.com>
2023-01-18 03:24:50 +0530

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,18 +1,21 @@
 "use strict";
-var age = 25;
-age = true;
-console.log(age);
-age = 'hello';
-console.log(age);
-age = { name: 'luigi' };
-console.log(age);
-var mixed = [];
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-var ninja;
-ninja = { name: 'yoshi', age: 25 };
-console.log(ninja);
-ninja = { name: 25, age: 'yoshi' };
-console.log(ninja);
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
+//
+// greet = 'hello';
+//
+// greet = () => {
+//   console.log('hello, again');
+// }
+var add = function (a, b, c /*?*/) {
+    if (c === void 0) { c /*?*/ = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 'ninja');
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+console.log(result);
